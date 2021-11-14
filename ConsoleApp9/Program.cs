62e57aef20ed3bbd7edc7db4904fd1b186f90a19
Kushagra23d <kushagra.23.cse@gmail.com>
2021-11-14 22:00:51 +0530

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Space1
+{
+    class H1
+    {
+        int val;
+        public void tes(int val)
+        {
+            this.val = val;
+            Console.WriteLine("Hello val: {0} ", val);
+        }
+
+        public int s()
+        {
+            return val;
+        }
+    }
+}
+
+
+
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Space1.H1 h = new Space1.H1();
+            Console.WriteLine("************************************************");
+
+            h.tes(5);
+
+            Console.WriteLine("************************************************");
+
+
+            Console.WriteLine("value of s: {0} ", h.s());
+
+            Console.WriteLine("************************************************");
+
+
+        }
+    }
+}
